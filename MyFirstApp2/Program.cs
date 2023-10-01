8ac4823f33bf8e252a264e5b97f943957eadff53
Merilin P <merilin.pruulrtktk.ee>
2023-10-01 20:28:18 +0300

    create new app

---
+++ MyFirstApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nimi
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string username = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + username + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {username} !");
